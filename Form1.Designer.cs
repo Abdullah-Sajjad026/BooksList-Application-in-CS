10764addf6bab02917e7ace2c9941a6d577698a2
Abdullah Sajjad <abdullahsa026@gmail.com>
2022-01-19 12:13:55 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CRUD_Application
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.authorBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.reviewBox = new System.Windows.Forms.TextBox();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(31, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book Name:";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(153, 105);
+            this.nameBox.Multiline = true;
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(204, 28);
+            this.nameBox.TabIndex = 0;
+            // 
+            // addBtn
+            // 
+            this.addBtn.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.addBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.addBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBtn.ForeColor = System.Drawing.Color.White;
+            this.addBtn.Location = new System.Drawing.Point(47, 262);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(90, 38);
+            this.addBtn.TabIndex = 3;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = false;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(396, 91);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(451, 209);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(31, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Author Name:";
+            // 
+            // authorBox
+            // 
+            this.authorBox.Location = new System.Drawing.Point(153, 149);
+            this.authorBox.Multiline = true;
+            this.authorBox.Name = "authorBox";
+            this.authorBox.Size = new System.Drawing.Size(204, 28);
+            this.authorBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(31, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Review:";
+            // 
+            // reviewBox
+            // 
+            this.reviewBox.Location = new System.Drawing.Point(153, 190);
+            this.reviewBox.Multiline = true;
+            this.reviewBox.Name = "reviewBox";
+            this.reviewBox.Size = new System.Drawing.Size(204, 28);
+            this.reviewBox.TabIndex = 2;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.BackColor = System.Drawing.Color.DarkKhaki;
+            this.updateBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.updateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateBtn.ForeColor = System.Drawing.Color.White;
+            this.updateBtn.Location = new System.Drawing.Point(145, 262);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(90, 38);
+            this.updateBtn.TabIndex = 4;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseVisualStyleBackColor = false;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.BackColor = System.Drawing.Color.DarkRed;
+            this.deleteBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.deleteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteBtn.ForeColor = System.Drawing.Color.White;
+            this.deleteBtn.Location = new System.Drawing.Point(241, 262);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(90, 38);
+            this.deleteBtn.TabIndex = 5;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = false;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Candara", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.MediumSlateBlue;
+            this.label4.Location = new System.Drawing.Point(344, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(195, 33);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Read Books List";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(882, 360);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.reviewBox);
+            this.Controls.Add(this.authorBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUD Application";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox authorBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox reviewBox;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUD_Application
+{
+    public partial class MainForm : Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        DataTable dt;
+        int Id = -1;
+
+        string connectionString = "Data Source=localhost; Initial Catalog= ReadBooksListDB; Integrated Security=true;";
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+            displayData();
+            clearForm();
+        }
+
+       public void displayData()
+        {
+            dt = new DataTable();
+            conn.Open();
+            adapter = new SqlDataAdapter("SELECT * FROM BooksList", conn);
+            adapter.Fill(dt);
+            conn.Close();
+            dataGridView1.DataSource = dt;
+        }
+        public void clearForm()
+        {
+            nameBox.Text = "";
+            authorBox.Text = "";
+            reviewBox.Text = "";
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Id = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            nameBox.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            authorBox.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            reviewBox.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            if(nameBox.Text == "" || authorBox.Text == "")
+            {
+                MessageBox.Show("Please fill required fields.");
+            }
+            else
+            {
+                try
+                {
+                    conn.Open();
+                    cmd = new SqlCommand($"INSERT INTO BooksList (Book_Name, Book_Author, Book_Review) values ('{nameBox.Text}', '{authorBox.Text}', '{reviewBox.Text}')", conn);
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    clearForm();
+                    displayData();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+     
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            if(nameBox.Text == "" || authorBox.Text == "" || Id == -1)
+            {
+                MessageBox.Show("Double-Click a record to update it.");
+            }
+            else
+            {
+                try
+                {
+                    conn.Open();
+                    cmd = new SqlCommand($"UPDATE BooksList SET Book_Name = '{nameBox.Text}', Book_Author = '{authorBox.Text}', Book_Review = '{reviewBox.Text}' WHERE Id = {Id};", conn);
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    Id = -1;
+                    clearForm();
+                    displayData();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (nameBox.Text == "" || authorBox.Text == "" || Id == -1)
+            {
+                MessageBox.Show("Double-Click a record to delete it.");
+            }
+            else
+            {
+                try
+                {
+                    conn.Open();
+                    cmd = new SqlCommand($"DELETE FROM BooksList WHERE Id={Id}", conn);
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    Id = -1;
+                    clearForm();
+                    displayData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
